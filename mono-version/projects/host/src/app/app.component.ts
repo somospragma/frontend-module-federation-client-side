fd9a985a821e70bcbbd9fe071ccc05222b7ce4ce
David Nicolas Sanchez Sendoya <david.sanchez@pragma.com.co>
2024-04-28 18:44:15 -0500

    feat: add prime ng

--- mono-version/projects/host/src/app/app.component.ts
+++ mono-version/projects/host/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+declare var require: any;
+const packageJson = require('../../../../package.json');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'host';
+  title = 'Host';
+  ngVersion = packageJson.dependencies['@angular/core'];
+  primeNGVersion = packageJson.dependencies['primeng'];
 }

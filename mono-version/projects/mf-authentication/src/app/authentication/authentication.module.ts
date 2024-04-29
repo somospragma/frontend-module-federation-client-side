e7250842537ede7156b17c39fe7137904562415b
David Nicolas Sanchez Sendoya <david.sanchez@pragma.com.co>
2024-04-28 19:01:30 -0500

    feat: add button to equal NF mono versión

--- mono-version/projects/mf-authentication/src/app/authentication/authentication.module.ts
+++ mono-version/projects/mf-authentication/src/app/authentication/authentication.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { PageCComponent } from './page-c/page-c.component';
 import { PageDComponent } from './page-d/page-d.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [PageCComponent, PageDComponent],
-  imports: [CommonModule, AuthenticationRoutingModule],
+  imports: [CommonModule, AuthenticationRoutingModule, ButtonModule],
 })
 export class AuthenticationModule {}
